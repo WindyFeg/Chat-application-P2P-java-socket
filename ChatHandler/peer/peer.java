59ac55db2487b40a34322a69b1b5c1b8aeda9a3e
WindyFeg <phongspeed7@yahoo.com.vn>
2022-11-23 17:17:31 +0700

    just need two UI file

---
+++ ChatHandler/peer/peer.java
@@ -0,0 +1,5 @@
+public class peer {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ ChatHandler/sever/sever.java
@@ -0,0 +1,7 @@
+package ChatHandler.sever;
+
+public class sever {
+    public static void main(String[] args) {
+        
+    }
+}
